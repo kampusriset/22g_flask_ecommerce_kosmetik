9dbd39f5fc48414b09034e6b122c9f41ca1bda91
ZbluRiz <rizkybayu1254@gmail.com>
2025-02-20 11:09:59 +0700

    menambahkan folder team

---
+++ Team/bayu.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Bayu Rizky Utomo"
+    nim = "2213010423"
+    peran = "Mengerjakan Fitur CRUD dan authentikasi login "
+    perkenalan(nama, nim, peran)
