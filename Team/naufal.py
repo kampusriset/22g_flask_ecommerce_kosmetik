75275be1784f1740065c84b05362e024b7e0abc0
Orcaln <naufalhanan78@gmail.com>
2025-02-20 13:13:25 +0800

    Menambahkan flowchart dan naufal.py

---
+++ Team/naufal.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Naufal Hanan Jati Asmara"
+    nim = "2213010413"
+    peran = "Mengerjakan Alur dan Template "
+    perkenalan(nama, nim, peran)
